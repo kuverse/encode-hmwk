919faefc78bd142efc19fd23cd4e7fb73a4ba830
kuverse <kucollectionnft@gmail.com>
2024-11-25 15:13:33 +0700

    initial commit

--- backend/nestbackend/src/app.service.ts
+++ backend/nestbackend/src/app.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import * as tokenJson from './assets/MyToken.json';
-import { createPublicClient, http, Address, formatEther } from 'viem';
+import {
+  createPublicClient,
+  http,
+  Address,
+  formatEther,
+  createWalletClient,
+} from 'viem';
 import { sepolia } from 'viem/chains';
 import { ConfigService } from '@nestjs/config';
+import { privateKeyToAccount } from 'viem/accounts';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  publicClient;
+  walletClient;
+
+  constructor(private configService: ConfigService) {
+    const apiKey = this.configService.get<Address>('ALCHEMY_API_KEY');
+    this.publicClient = createPublicClient({
+      chain: sepolia,
+      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`),
+    });
+    const account = privateKeyToAccount(
+      `0x${this.configService.get<string>('PRIVATE_KEY')}`,
+    );
+    this.walletClient = createWalletClient({
+      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`),
+      chain: sepolia,
+      account: account,
+    });
+  }
 
   getHello(): string {
     return 'Hello World!';
@@ -17,12 +41,7 @@ export class AppService {
   }
 
   async getTokenName(): Promise<string> {
-    const apiKey = this.configService.get<Address>('ALCHEMY_API_KEY');
-    const publicClient = createPublicClient({
-      chain: sepolia,
-      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`),
-    });
-    const name = await publicClient.readContract({
+    const name = await this.publicClient.readContract({
       address: this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'name',
@@ -31,21 +50,59 @@ export class AppService {
   }
 
   async getTotalSupply() {
-    const apiKey = this.configService.get<Address>('ALCHEMY_API_KEY');
-    const publicClient = createPublicClient({
-      chain: sepolia,
-      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`),
-    });
-    const symbol = await publicClient.readContract({
+    const symbol = await this.publicClient.readContract({
       address: this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'symbol',
     });
-    const totalSupply = await publicClient.readContract({
+    const totalSupply = await this.publicClient.readContract({
       address: this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'totalSupply',
     });
     return `${formatEther(totalSupply as bigint)} ${symbol}`;
+  }
+
+  async getTokenBalance(address: string) {
+    const symbol = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: 'symbol',
+    });
+    const balanceOf = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: 'balanceOf',
+      args: [address],
+    });
+    return `${formatEther(balanceOf as bigint)} ${symbol}`;
+  }
+
+  async getTransactionReceipt(hash: string) {
+    const tx = await this.publicClient.getTransactionReceipt({ hash });
+    return `Transaction status: ${tx.status}, Block number: ${tx.blockNumber}`;
+  }
+
+  getServerWalletAddress() {
+    return this.walletClient.account.address;
+  }
+
+  async checkMinterRole(address: string) {
+    const MINTER_ROLE = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: 'MINTER_ROLE',
+    });
+    const hasRole = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: 'hasRole',
+      args: [MINTER_ROLE, address],
+    });
+    return `The address ${address} ${hasRole ? 'has' : 'does not have'} the role ${MINTER_ROLE}`;
+  }
+
+  async mintTokens(address: string) {
+    return address;
   }
 }
